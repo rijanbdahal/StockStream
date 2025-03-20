0335b747c9860ace964c7df1c133ecbf7ac11e22
rijan <rijan.dahal@dcmail.ca>
2025-03-20 14:40:59 -0400

    main

--- server/server.js
+++ server/server.js
@@ -86,7 +86,8 @@ app.all("*", (req, res) => {
     res.status(404).send("Route not found");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
